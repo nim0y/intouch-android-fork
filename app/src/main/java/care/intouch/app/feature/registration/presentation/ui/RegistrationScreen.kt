efef6169b74b519c5e5d5c460987af13eaa53739
ImNia <agnia-kimi1996@mail.ru>
2024-05-06 22:44:21 +0700

    Added header in registration screen

--- app/src/main/java/care/intouch/app/feature/registration/presentation/ui/RegistrationScreen.kt
+++ app/src/main/java/care/intouch/app/feature/registration/presentation/ui/RegistrationScreen.kt
@@ -1,15 +1,28 @@
 package care.intouch.app.feature.registration.presentation.ui
 
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import care.intouch.uikit.R
 import care.intouch.app.feature.registration.presentation.viewmodel.RegistrationEvent
 import care.intouch.app.feature.registration.presentation.viewmodel.RegistrationViewModel
+import care.intouch.uikit.theme.InTouchTheme
 
 @Composable
 fun RegistrationScreen(
@@ -23,21 +36,43 @@ fun RegistrationScreen(
     viewModel.onEvent(RegistrationEvent.OnGetUserInfo(userId, token))
 
     Scaffold { paddingValues ->
-        when(state.value.screenState) {
+        when (state.value.screenState) {
             RegistrationScreenState.Loading -> {
 
             }
 
             RegistrationScreenState.Registration -> {
-                SetPasswordScreen(
+                Column(
                     modifier = Modifier
                         .fillMaxSize()
+                        .background(InTouchTheme.colors.white)
                         .padding(paddingValues),
-                    userName = state.value.userName,
-                    errorPassword = state.value.errorPassword,
-                    errorPasswordText = state.value.errorPasswordText,
-                    onEvent = viewModel::onEvent
-                )
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(172.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Image(
+                            modifier = Modifier.fillMaxWidth(),
+                            contentScale = ContentScale.FillBounds,
+                            painter = painterResource(id = R.drawable.head_background_small),
+                            contentDescription = null
+                        )
+                        Image(
+                            painter = painterResource(id = R.drawable.head_logo),
+                            contentDescription = null
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(48.dp))
+                    SetPasswordScreen(
+                        userName = state.value.userName,
+                        errorPassword = state.value.errorPassword,
+                        errorPasswordText = state.value.errorPasswordText,
+                        onEvent = viewModel::onEvent
+                    )
+                }
             }
         }
     }
