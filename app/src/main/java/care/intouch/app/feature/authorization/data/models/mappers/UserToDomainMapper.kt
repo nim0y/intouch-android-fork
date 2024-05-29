542c3af7aa8073d86d61c93eae32b0bc20ab8302
Ilnur Valiev <102486283+JudjinGM@users.noreply.github.com>
2024-05-29 09:53:36 +0400

    fix according PR review 3

--- app/src/main/java/care/intouch/app/feature/authorization/data/models/mappers/UserToDomainMapper.kt
+++ app/src/main/java/care/intouch/app/feature/authorization/data/models/mappers/UserToDomainMapper.kt
@@ -6,22 +6,24 @@ import javax.inject.Inject
 
 class UserToDomainMapper @Inject constructor() {
     fun toUser(response: UserResponse): User {
-        return User(
-            id = response.id ?: DEFAULT_ID,
-            firstName = response.firstName ?: DEFAULT_FIRST_NAME,
-            lastName = response.lastName ?: DEFAULT_LAST_NAME,
-            email = response.email ?: DEFAULT_EMAIL,
-            acceptPolicy = response.acceptPolicy ?: DEFAULT_ACCEPT_POLICY,
-            newEmailTemp = response.newEmailTemp ?: DEFAULT_NEW_EMAIL_TEMP,
-            newEmailChanging = response.newEmailChanging ?: DEFAULT_NEW_EMAIL_CHANGING
-        )
+        with(response) {
+            if (id == null || firstName == null || email == null) {
+                throw IllegalArgumentException("UserResponse is missing required fields: id, firstName, or email.")
+            }
+            return User(
+                id = id,
+                firstName = firstName,
+                email = email,
+                lastName = lastName ?: DEFAULT_LAST_NAME,
+                acceptPolicy = acceptPolicy ?: DEFAULT_ACCEPT_POLICY,
+                newEmailTemp = newEmailTemp ?: DEFAULT_NEW_EMAIL_TEMP,
+                newEmailChanging = newEmailChanging ?: DEFAULT_NEW_EMAIL_CHANGING
+            )
+        }
     }
 
     companion object {
-        private const val DEFAULT_ID = -1
-        private const val DEFAULT_FIRST_NAME = ""
         private const val DEFAULT_LAST_NAME = ""
-        private const val DEFAULT_EMAIL = ""
         private const val DEFAULT_ACCEPT_POLICY = false
         private const val DEFAULT_NEW_EMAIL_TEMP = ""
         private const val DEFAULT_NEW_EMAIL_CHANGING = false
