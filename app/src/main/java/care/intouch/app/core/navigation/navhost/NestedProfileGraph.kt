6b919a231c49ceb010c50a97f4616089eb7fd731
Menta1 <exen1993@gmail.com>
2024-06-22 00:19:33 +0300

    Corrected navigation in profile

--- app/src/main/java/care/intouch/app/core/navigation/navhost/NestedProfileGraph.kt
+++ app/src/main/java/care/intouch/app/core/navigation/navhost/NestedProfileGraph.kt
@@ -2,18 +2,17 @@ package care.intouch.app.core.navigation.navhost
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
 import androidx.navigation.navigation
 import care.intouch.app.core.navigation.MainNav
 import care.intouch.app.core.navigation.PinCodeChange
 import care.intouch.app.core.navigation.PinCodeConfirm
-import care.intouch.app.core.navigation.PinCodeConfirmation
 import care.intouch.app.core.navigation.ProfileRouteBranch
 import care.intouch.app.core.navigation.SuccessfulPinCodeChange
 import care.intouch.app.feature.pinCode.ui.confirm.PinCodeConfirmationScreen
 import care.intouch.app.feature.pinCode.ui.install.PinCodeInstallationScreen
+import care.intouch.app.feature.profile.presentation.ui.PinCodeChangeScreen
+import care.intouch.app.feature.profile.presentation.ui.PinCodeConfirmScreen
 import care.intouch.app.feature.profile.presentation.ui.SuccessfulPinCodeChangeScreen
 
 fun NavGraphBuilder.addNestedProfileGraph(
@@ -24,40 +23,45 @@ fun NavGraphBuilder.addNestedProfileGraph(
     ) {
 
         composable(route = PinCodeChange.route) {
-            PinCodeInstallationScreen(onSaveClick = { argument ->
-                navController.navigate(route = PinCodeConfirm.route + "/$argument")
-            }, onSkipClick = {
-                navController.navigate(route = MainNav.route) {
-                    popUpTo(navController.graph.startDestinationId) {
-                        inclusive = true
+            PinCodeChangeScreen(
+                pinCodeEntered = {
+                    navController.navigate(route = PinCodeConfirm.route)
+                },
+                exit = {
+                    navController.navigate(route = MainNav.route) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            inclusive = true
+                        }
                     }
                 }
-            })
+            )
         }
 
-        composable(
-            route = PinCodeConfirmation.route + "/{pinCodeInst}",
-            arguments = listOf(navArgument("pinCodeInst") { type = NavType.StringType })
-        ) {
-            PinCodeConfirmationScreen(onSaveClick = {
-                navController.navigate(route = SuccessfulPinCodeChange.route)
-            }, onSkipClick = {
-                navController.navigate(route = MainNav.route) {
-                    popUpTo(navController.graph.startDestinationId) {
-                        inclusive = true
+        composable(route = PinCodeConfirm.route) {
+            PinCodeConfirmScreen(
+                pinCodeConfirm = {
+                    navController.navigate(route = SuccessfulPinCodeChange.route)
+                },
+                exit = {
+                    navController.navigate(route = MainNav.route) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            inclusive = true
+                        }
                     }
                 }
-            })
+            )
         }
 
         composable(route = SuccessfulPinCodeChange.route) {
-            SuccessfulPinCodeChangeScreen(onBackToHome = {
-                navController.navigate(route = MainNav.route) {
-                    popUpTo(navController.graph.startDestinationId) {
-                        inclusive = true
+            SuccessfulPinCodeChangeScreen(
+                onBackToHome = {
+                    navController.navigate(route = MainNav.route) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            inclusive = true
+                        }
                     }
                 }
-            })
+            )
         }
     }
 }
