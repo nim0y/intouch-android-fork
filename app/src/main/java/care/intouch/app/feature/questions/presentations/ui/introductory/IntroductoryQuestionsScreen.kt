dd7b2ecac98a1be6880d066957aaa08eeb2f9058
DaniilKosmachev <fame37@yandex.ru>
2024-07-02 20:57:44 +0300

    init

---
+++ app/src/main/java/care/intouch/app/feature/questions/presentations/ui/introductory/IntroductoryQuestionsScreen.kt
@@ -0,0 +1,18 @@
+package care.intouch.app.feature.questions.presentations.ui.introductory
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import care.intouch.uikit.theme.InTouchTheme
+
+@Composable
+private fun IntroductoryQuestionsScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun IntroductoryQuestionsCreenPreview() {
+    InTouchTheme {
+        IntroductoryQuestionsScreen()
+    }
+}
