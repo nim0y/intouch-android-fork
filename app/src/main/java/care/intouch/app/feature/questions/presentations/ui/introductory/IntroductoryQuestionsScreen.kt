5c5b14b9b748a98cead75a310f5deb081c23d8f6
DaniilKosmachev <fame37@yandex.ru>
2024-07-02 22:00:56 +0300

    Add string resource Add header on IntroductoryQuestionsScreen

--- app/src/main/java/care/intouch/app/feature/questions/presentations/ui/introductory/IntroductoryQuestionsScreen.kt
+++ app/src/main/java/care/intouch/app/feature/questions/presentations/ui/introductory/IntroductoryQuestionsScreen.kt
@@ -1,17 +1,38 @@
 package care.intouch.app.feature.questions.presentations.ui.introductory
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import care.intouch.uikit.common.StringVO
 import care.intouch.uikit.theme.InTouchTheme
+import care.intouch.uikit.ui.profile.TopPanel
 
 @Composable
 private fun IntroductoryQuestionsScreen() {
+    Box (
+        modifier = Modifier.background(InTouchTheme.colors.mainBlue)
+    ) {
+        Column (
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Spacer(modifier = Modifier.height(55.dp))
+            TopPanel(text = StringVO.Resource(resId = care.intouch.app.R.string.socratic_dialogue))
+            Spacer(modifier = Modifier.height(28.dp))
 
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun IntroductoryQuestionsCreenPreview() {
+fun IntroductoryQuestionsScreenPreview() {
     InTouchTheme {
         IntroductoryQuestionsScreen()
     }
