a47a7b2260903fe325fdda3b49330c08505e7c53
DaniilKosmachev <fame37@yandex.ru>
2024-07-03 19:54:01 +0300

    Add new UI element on IntroductoryQuestionsScreen

--- app/src/main/java/care/intouch/app/feature/questions/presentations/ui/introductory/IntroductoryQuestionsScreen.kt
+++ app/src/main/java/care/intouch/app/feature/questions/presentations/ui/introductory/IntroductoryQuestionsScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import care.intouch.uikit.common.StringVO
