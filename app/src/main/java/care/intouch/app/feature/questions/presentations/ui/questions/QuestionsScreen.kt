d0bce5144c456eb4b80c9b6d9224c2d78188a997
DaniilKosmachev <fame37@yandex.ru>
2024-07-06 17:31:04 +0300

    Add new jpg image - dunning_kruger_effect_image.jpg Add new string resource Add dunning-kruger effect image field

--- app/src/main/java/care/intouch/app/feature/questions/presentations/ui/questions/QuestionsScreen.kt
+++ app/src/main/java/care/intouch/app/feature/questions/presentations/ui/questions/QuestionsScreen.kt
@@ -1,5 +1,6 @@
 package care.intouch.app.feature.questions.presentations.ui.questions
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,10 +10,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import care.intouch.app.R
@@ -89,6 +94,29 @@ private fun QuestionsScreen() {
                 bottomSubtitleText = StringVO.Resource(R.string.professional_development_snapshot),
                 bottomCaptionText = StringVO.Resource(R.string.professional_development_snapshot_description)
                 )
+            Spacer(modifier = Modifier.height(40.dp))
+            Box(modifier = Modifier
+                .background(color = InTouchTheme.colors.input85, shape = RoundedCornerShape(8.dp))
+                .fillMaxWidth()
+            ) {
+                Column (modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .padding(top = 14.dp, bottom = 20.dp)) {
+                    Text(
+                        text = StringVO.Resource(R.string.dunning_kruger_effect).value(),
+                        style = InTouchTheme.typography.bodySemibold,
+                        color = InTouchTheme.colors.textGreen
+                    )
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Image(
+                        painter = painterResource(id = care.intouch.uikit.R.drawable.dunning_kruger_effect_image),
+                        contentDescription = "task image on introductory questions screen",
+                        contentScale = ContentScale.Fit
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(34.dp))
+
         }
     }
 }
