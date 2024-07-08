5dcaf55b8236791965334a80737abe2a8fcf171c
DaniilKosmachev <fame37@yandex.ru>
2024-07-08 20:37:09 +0300

    Fix checked radioButton group

--- uikit/src/main/java/care/intouch/uikit/ui/questions/TextFieldWithCheckbox.kt
+++ uikit/src/main/java/care/intouch/uikit/ui/questions/TextFieldWithCheckbox.kt
@@ -14,7 +14,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
@@ -42,7 +42,7 @@ fun TextFieldWithCheckbox(
     backgroundColor: Color = InTouchTheme.colors.input85,
 ) {
     val items = listOf(checkboxText, secondCheckboxText, thirdCheckboxText, fourthCheckboxText)
-    val (selected, setSelected) = remember { mutableStateOf(items[0]) }
+    val (selected, setSelected) = rememberSaveable { mutableStateOf(items[0]) }
 
     Column(
         modifier = modifier.width(MultilineTextFieldDefaults.MinWidth)
