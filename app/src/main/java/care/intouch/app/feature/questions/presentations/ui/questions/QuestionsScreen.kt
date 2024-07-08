5d1595c34ba7823bbcd92a5109500967cb167dc5
DaniilKosmachev <fame37@yandex.ru>
2024-07-08 22:00:23 +0300

    Fix text field behavior

--- app/src/main/java/care/intouch/app/feature/questions/presentations/ui/questions/QuestionsScreen.kt
+++ app/src/main/java/care/intouch/app/feature/questions/presentations/ui/questions/QuestionsScreen.kt
@@ -2,6 +2,7 @@ package care.intouch.app.feature.questions.presentations.ui.questions
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,13 +15,21 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import care.intouch.app.R
@@ -37,10 +46,15 @@ import care.intouch.uikit.ui.toggle.Toggle
 
 @Composable
 private fun QuestionsScreen() {
+    var answerText by remember { mutableStateOf("") }
+    val systemKeyboardController = LocalSoftwareKeyboardController.current
     Box(
         modifier = Modifier
             .background(InTouchTheme.colors.mainBlue)
             .verticalScroll(rememberScrollState())
+            .clickable {
+                systemKeyboardController?.hide()
+            }
     ) {
         Column (
             modifier = Modifier
@@ -58,12 +72,19 @@ private fun QuestionsScreen() {
             MultilineTextField(
                 subtitleText = StringVO.Resource(R.string.motivates_question),
                 captionText = StringVO.Resource(R.string.inscribe_motivates_question),
-                value = "",
+                value = answerText,
                 modifier = Modifier.fillMaxWidth(),
                 hint = StringVO.Resource(R.string.write_your_answer_here),
-                onValueChange = { },
+                onValueChange = {
+                    answerText = it
+                },
                 isError = false,
-                enabled = true)
+                enabled = true,
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    systemKeyboardController?.hide()
+                })
+            )
             Spacer(modifier = Modifier.height(40.dp))
             TextFieldWithCheckbox(
                 modifier = Modifier.fillMaxWidth(),
