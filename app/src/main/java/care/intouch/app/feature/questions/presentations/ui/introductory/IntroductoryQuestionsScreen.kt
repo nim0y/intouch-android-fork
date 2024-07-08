2fb8f8bb6c8914af3cf25b4332e6ce4fda717721
DaniilKosmachev <fame37@yandex.ru>
2024-07-08 23:31:06 +0300

    Fix IntroductoryQuestionsScreen UI Create a mock IntroductoryState Create view model for IntroductoryQuestionsScreen

--- app/src/main/java/care/intouch/app/feature/questions/presentations/ui/introductory/IntroductoryQuestionsScreen.kt
+++ app/src/main/java/care/intouch/app/feature/questions/presentations/ui/introductory/IntroductoryQuestionsScreen.kt
@@ -9,9 +9,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -19,19 +23,46 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import care.intouch.app.R
+import care.intouch.app.feature.questions.presentations.ui.models.IntroductoryState
 import care.intouch.uikit.common.StringVO
 import care.intouch.uikit.theme.InTouchTheme
 import care.intouch.uikit.ui.buttons.IntouchButton
 import care.intouch.uikit.ui.questions.TopPanel
 
 @Composable
-private fun IntroductoryQuestionsScreen() {
+fun IntroductoryQuestionsScreen(
+    onBackClick: () -> Unit,
+    onNextClick: () -> Unit,
+    viewModel: IntroductoryViewModel = hiltViewModel()
+) {
+    val state by viewModel.state.collectAsState()
+
+    IntroductoryQuestionsScreen(
+        onBackClick = onBackClick,
+        onNextClick = onNextClick,
+        onEvent = {
+            viewModel.onEvent()
+        },
+        state = state
+    )
+}
+
+@Composable
+private fun IntroductoryQuestionsScreen(
+    onBackClick: () -> Unit,
+    onNextClick: () -> Unit,
+    onEvent: () -> Unit,
+    state: IntroductoryState
+) {
     Box (
         modifier = Modifier.background(InTouchTheme.colors.mainBlue)
     ) {
         Column (
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Spacer(modifier = Modifier.height(55.dp))
@@ -52,7 +83,8 @@ private fun IntroductoryQuestionsScreen() {
                 Image(
                     painter = painterResource(id = care.intouch.uikit.R.drawable.task_image_mock),
                     contentDescription = "task image on introductory questions screen",
-                    contentScale = ContentScale.Fit
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier.fillMaxSize()
                 )
             }
             Spacer(modifier = Modifier.height(24.dp))
@@ -61,6 +93,7 @@ private fun IntroductoryQuestionsScreen() {
                 modifier = Modifier,
                 text = StringVO.Resource(R.string.next_button)
             )
+            Spacer(modifier = Modifier.height(40.dp))
         }
     }
 }
@@ -68,7 +101,13 @@ private fun IntroductoryQuestionsScreen() {
 @Preview(showBackground = true)
 @Composable
 fun IntroductoryQuestionsScreenPreview() {
+    val state = IntroductoryState("")
     InTouchTheme {
-        IntroductoryQuestionsScreen()
+        IntroductoryQuestionsScreen(
+            onBackClick = {},
+            onNextClick = {},
+            onEvent = {},
+            state = state
+        )
     }
 }
