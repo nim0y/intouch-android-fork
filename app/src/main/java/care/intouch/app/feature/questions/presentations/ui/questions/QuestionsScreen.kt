bf23250257d603025503eaba8cda628e28781786
DaniilKosmachev <fame37@yandex.ru>
2024-07-12 19:08:02 +0300

    Fix close dialog position

--- app/src/main/java/care/intouch/app/feature/questions/presentations/ui/questions/QuestionsScreen.kt
+++ app/src/main/java/care/intouch/app/feature/questions/presentations/ui/questions/QuestionsScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -213,6 +214,7 @@ private fun QuestionsScreen(
     if (showClosingTaskWithoutSaveDialog) {
         Surface(
             modifier = Modifier
+                .wrapContentHeight(align = Alignment.Top)
                 .padding(horizontal = 28.dp)
                 .alpha(1F)
                 .padding(top = 100.dp)
