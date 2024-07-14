f279042fdd60da1eb4b692a5a45055f664c3bf2f
Babaylov Anton <babaylov-ayu@mail.ru>
2024-07-14 22:39:32 +0500

    In progress

--- app/src/main/java/care/intouch/app/feature/questions/di/AssignmentsModule.kt
+++ app/src/main/java/care/intouch/app/feature/questions/di/AssignmentsModule.kt
@@ -1,5 +1,7 @@
 package care.intouch.app.feature.questions.di
 
+import care.intouch.app.feature.questions.data.impl.GetAssignmentsRepositoryImpl
+import care.intouch.app.feature.questions.domain.useCase.GetAssignmentsRepository
 import care.intouch.app.feature.questions.domain.useCase.GetAssignmentsUseCase
 import dagger.Binds
 import dagger.Module
@@ -12,4 +14,7 @@ interface AssignmentsModule {
 
     @Binds
     fun bindGetAssignmentsUseCase(impl: GetAssignmentsUseCase.Base): GetAssignmentsUseCase
+
+    @Binds
+    fun bindGetAssignmentsRepository(impl: GetAssignmentsRepositoryImpl): GetAssignmentsRepository
 }
