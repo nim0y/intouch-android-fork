0f5621930a4c7903900abe6d870a1a454e100da9
Babaylov7 <babaylov-ayu@mail.ru>
2024-07-15 14:14:45 +0500

    in progress

--- app/src/main/java/care/intouch/app/feature/questions/data/impl/GetAssignmentsRepositoryImpl.kt
+++ app/src/main/java/care/intouch/app/feature/questions/data/impl/GetAssignmentsRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package care.intouch.app.feature.questions.data.impl
 
+import care.intouch.app.feature.authorization.data.models.exception.AuthenticationException
+import care.intouch.app.feature.authorization.data.models.mappers.NetworkToUserExceptionMapper.Companion.COULD_NOT_CONVERT_TO_ERROR_RESPONSE
 import care.intouch.app.feature.common.data.models.exception.NetworkException
 import care.intouch.app.feature.questions.data.api.GetAssignmentsApi
 import care.intouch.app.feature.questions.data.converters.AssignmentsConvertor
@@ -25,11 +27,24 @@ class GetAssignmentsRepositoryImpl  @Inject constructor(
                     val response = handleErrorResponse<Assignments>(e.errorBody)
                     Result.failure(
                         NetworkException.BadRequest(
-                            response.de
+                            "Надо сюда поймать ошибку какую-нибудь",
+                            e.httpStatusCode
                         )
                     )
                 }
+                else -> {
+                    Result.failure(e)
+                }
             }
+        }
+    }
+
+    private inline fun <reified T> handleErrorResponse(errorMessage: String): T {
+        try {
+            return json.decodeFromString<T>(errorMessage)
+
+        } catch (e: Exception) {
+            throw AuthenticationException.Undefined(COULD_NOT_CONVERT_TO_ERROR_RESPONSE)
         }
     }
 }
