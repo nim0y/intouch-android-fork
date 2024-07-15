77a4dadd651f95c661ef1f172f752993926ddd2e
Anton <130746178+Babaylov7@users.noreply.github.com>
2024-07-15 13:52:45 +0500

    Feature/75 profile presentation domain data (#157)

--- app/src/main/java/care/intouch/app/core/navigation/navhost/AppNavHost.kt
+++ app/src/main/java/care/intouch/app/core/navigation/navhost/AppNavHost.kt
@@ -19,7 +19,7 @@ import care.intouch.app.core.navigation.QuestionsRouteBranch
 import care.intouch.app.feature.diary.presentation.ui.DiaryNoteScreen
 import care.intouch.app.feature.home.presentation.ui.HomeScreen
 import care.intouch.app.feature.plan.presentation.ui.PlanScreen
-import care.intouch.app.feature.profile.presentation.ui.profile.ProfileScreen
+import care.intouch.app.feature.profile.presentation.ui.profile.ui.ProfileScreen
 import care.intouch.app.feature.profile.presentation.ui.security.SecurityScreenInit
 
 @Composable
@@ -71,6 +71,9 @@ fun AppNavHost(
                 },
                 onChangePinCode = {
                     navController.navigate(route = ProfileRouteBranch.route)
+                },
+                onSingOut = {
+                    navController.navigate(route = Authentication.route)
                 }
             )
         }
